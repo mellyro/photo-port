de883f8147c541fd97c861f85fdaef97e99993c7
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-07-29 12:57:09 -0500

    add conditional rendering

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -41,16 +41,22 @@ function ContactForm() {
             <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+                    <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" defaultValue={email} onChange={handleChange} name="email" />
+                    <input type="email" defaultValue={email} onBlur={handleChange} name="email" />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange}/>
+                    <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange}/>
                 </div>
+
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
 
                 <button type="submit">Submit</button>
             </form>
