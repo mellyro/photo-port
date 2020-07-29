c7bfd41d5e950d044c851cfe78d9efaf4129c5b4
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-07-29 10:33:31 -0500

    add changes

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -22,21 +22,18 @@ function Nav(props) {
             <nav>
                 <ul className="flex-row">
                 <li className="mx-2">
-                    <a data-testid="about" href="#about">
-                    About me
-                    </a>
+                    <a data-testid="about" href="#about">About me</a>
                 </li>
                 <li>
                     <span>Contact</span>
                 </li>
                 {categories.map((category) => (
-                    <li
-                    className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`}
-                    key={category.name}
-                    >
+                    <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`}
+                    key={category.name}>
                     <span onClick={() => {
                         setCurrentCategory(category)
-                    }}> 
+                    }}
+                    > 
                     {capitalizeFirstLetter(category.name)}
                     </span>
                     </li>
